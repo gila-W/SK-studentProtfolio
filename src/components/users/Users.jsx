f084671c855cddf3661879cee3a337e659c99eb3
gila-W <gw38452@gmail.com>
2025-03-05 11:52:31 +0200

    start fetch lists from firebase

---
+++ src/components/users/Users.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useQuery } from "react-query"
+import { useState } from 'react';
+import db from '../../firebase';
+import {
+    collection,
+    getDocs,
+} from 'firebase/firestore';
+
+
+const fetchUsers = async () => {
+    const querySnapshot = await getDocs(collection(db, 'systemUsers'));
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  };
+export const Users=()=> {
+    const { data: users = [], error, isLoading } = useQuery("systemUsers", fetchUsers);
+
+   if (isLoading) return <div>Loading...</div>;
+   if (error) return <div>Error loading users</div>;  return (
+    <div>Users
+    {users?.map((u) => <li key={u.id}>{u.userName}</li>)}
+  </div>
+  )
+}
