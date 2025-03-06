a641a1b92e0a01047d4d5f3143f520ff32cff083
gila-W <gw38452@gmail.com>
2025-03-06 15:33:26 +0200

    start with credit card prigress

---
+++ src/components/newStudent/studentEducation/StudentEducationClub.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './StudentEducation.css'
+export const StudentEducationClub=()=> {
+  return (
+    <div></div>
+  )
+}
--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -4,16 +4,25 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "../components/homePage/HomePage";
 import { StudentDetails } from "../components/studentDetails/StudentDetails";
 import { LogIn } from "../components/logIn/LogIn";
+import { NewStudentProgressBar } from "../components/newStudent/newStudentProgressBar/NewStudentProgressBar";
 
 export const Routing = () => {
   return (
-    <div style={{ backgroundColor: "yellow", display: "revert" }}>
       <Routes>
         <Route path="/" element={<LogIn />}></Route>
         <Route path="/home-page" element={<HomePage />}></Route>
         <Route path="/student-details" element={<StudentDetails />}></Route>
-      </Routes>
-    </div>
+  
+        <Route 
+          path="/new-student" 
+          element={
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                <NewStudentProgressBar />
+              </div>
+            </div>
+          } 
+        /> {/* עטיפת הקומפוננטה של תלמיד חדש בסטייל */}      </Routes>
   );
 };
 
